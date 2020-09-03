54a98b3ba2bdb875d435cf6884199a52e4f8c77f
jdelledonne <j.delledonne4@gmail.com>
2020-09-03 16:42:45 -0400

    Added starter code

---
+++ app/app.js
@@ -0,0 +1,44 @@
+/* Creating the module, passing array of other dependent modules */
+angular.module('app', ['ngMaterial', 'ngMessages']);
+
+/*--------------------- Home Component ---------------------*/
+const home = {
+    templateUrl: './home/home.html',
+    controller: 'HomeController'
+}
+
+// Home Component with Routing (Routed / Stateful), registering a component to the module
+angular.module('app').component('home', home);
+
+// Home Controller with dependency injection using the array method
+angular.module('app').controller('HomeController', ['ExampleService', function (ExampleService) {
+    this.exampleVariable = "I am the home component!";
+}]);
+/*--------------------- Home Component ---------------------*/
+
+/*--------------------- Settings Component ---------------------*/
+const settings = {
+    templateUrl: '',
+    controller: 'SettingsController'
+}
+
+// Settings Component with Routing (Routed / Stateful)
+angular.module('app').component('settings', settings)
+
+// Settings Controller with dependency injection using $inject method
+function SettingsController(ExampleService) {
+
+}
+SettingsController.$inject = ['ExampleService'];
+angular.module('app').controller('SettingsController', SettingsController);
+/*--------------------- Settings Component ---------------------*/
+
+/*--------------------- Example Service ---------------------*/
+function ExampleService() {
+    // Services are Singletons
+    // Properties
+    // Methods
+}
+angular.module('app').service('ExampleService', ExampleService)
+/*--------------------- Example Service ---------------------*/
+
